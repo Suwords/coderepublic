2b777ff8e3abc57b4522e476be06ee2948515df6
苏辞 <yangp9702@gmail.com>
2022-10-29 21:22:28 +0800

    Spark

--- scala/src/main/scala/info/datasheep/spark/lesson06_rdd_over.scala
+++ scala/src/main/scala/info/datasheep/spark/lesson06_rdd_over.scala
@@ -29,6 +29,9 @@ object lesson06_rdd_over {
       (arrs(0).toInt, arrs(1).toInt, arrs(2).toInt, arr(1).toInt)
     })
 
+    implicit val sdfsdf = new Ordering[(Int, Int)] {
+                    override def compare(x: (Int, Int), y: (Int, Int)) = y._2.compareTo(x._2)
+                  }
     // 第一代
     // 用来groupByKey 容易OOM 且自己的算子实现了函数：去重、排序
     val grouped: RDD[((Int, Int), Iterable[(Int, Int)])] = data.map(t4 => ((t4._1, t4._2), (t4._3, t4._4))).groupByKey()
@@ -41,6 +44,7 @@ object lesson06_rdd_over {
         override def compare(x: (Int, Int), y: (Int, Int)) = y._2.compareTo(x._2)
       })
     })
+    println("----------第一代---------")
     res01.foreach(println)
 
     // 第二代
@@ -50,7 +54,8 @@ object lesson06_rdd_over {
     })
     val maped: RDD[((Int, Int), (Int, Int))] = reduced.map(t2 => ((t2._1._1, t2._1._2), (t2._1._3, t2._2)))
     val grouped2: RDD[((Int, Int), Iterable[(Int, Int)])] = maped.groupByKey()
-    grouped.mapValues(arr => arr.toList.sorted.take(2)).foreach(println)
+    println("----------第二代---------")
+    grouped2.mapValues(arr => arr.toList.sorted.take(2)).foreach(println)
 
     // 第三代
     // 用了groupByKey 容易OOM 取巧：用了reduceByKey去重，用了sortByKey排序
@@ -59,12 +64,14 @@ object lesson06_rdd_over {
     val reduced2: RDD[((Int, Int, Int), Int)] = sorted.map(t4 => ((t4._1, t4._2, t4._3), t4._4)).reduceByKey((x: Int, y: Int) => if (y > x) y else x)
     val maped2: RDD[((Int, Int), (Int, Int))] = reduced2.map(t2 => ((t2._1._1, t2._1._2), (t2._1._3, t2._2)))
     val grouped3: RDD[((Int, Int), Iterable[(Int, Int)])] = maped2.groupByKey()
+    println("----------第三代---------")
     grouped3.foreach(println)
 
     // 第四代
     // 用了 groupByKey 容易OOM 取巧：用了sortByKey排序 没有破坏多级shuffle的key的子集关系
     val sorted2: RDD[(Int, Int, Int, Int)] = data.sortBy(t4 => (t4._1, t4._2, t4._4), false)
     val grouped4: RDD[((Int, Int), Iterable[(Int, Int)])] = sorted2.map(t4 => ((t4._1, t4._2), (t4._3, t4._4))).groupByKey()
+    println("----------第四代---------")
     grouped4.foreach(println)
 
     // 第五代
@@ -76,10 +83,12 @@ object lesson06_rdd_over {
     val kv: RDD[((Int, Int), (Int, Int))] = data.map(t4 => ((t4._1, t4._2), (t4._3, t4._4)))
     val res: RDD[((Int, Int), Array[(Int, Int)])] = kv.combineByKey(
       // 第一条记录怎么放：
+      // (1, 38)
       (v1: (Int, Int)) => {
-        Array(v1, (0, 0), (0, 0))
+        Array(v1, (0, 0)) // ((1, 39), (0, 0))
       },
       // 第二条，以及后续的怎么放：
+      // ((1, 39), (0, 0)) (1, 40)
       (oldv: Array[(Int, Int)], newv: (Int, Int)) => {
         // 去重 排序
         var flg = 0 // 0,1,2 新进来的元素特征： 日 a）相同 1）温度大 2）温度小 日 b）不同
@@ -87,9 +96,7 @@ object lesson06_rdd_over {
           if (oldv(i)._1 == newv._1) {
             if (oldv(i)._2 < newv._2) {
               flg = 1
-              oldv(i) = newv
-            } else {
-              flg = 2
+              oldv(i) = newv  // ((1, 40), (0, 0))
             }
           }
         }
@@ -107,6 +114,30 @@ object lesson06_rdd_over {
         union.sorted
       }
     )
+    println("----------第五代---------")
     res.map(x => (x._1, x._2.toList)).foreach(println)
+
+
+    // map与mapvalues
+    val data1: RDD[String] = sc.parallelize(List(
+      "hello world",
+      "hello spark",
+      "hello world",
+      "hello hadoop",
+      "hello world",
+      "hello msb",
+      "hello world"
+    ))
+    val words: RDD[String] = data1.flatMap(_.split(" "))
+    val kv1: RDD[(String, Int)] = words.map((_,1))
+    val res1: RDD[(String, Int)] = kv1.reduceByKey(_+_)
+    val res001: RDD[(String, Int)] = res1.map(x=>(x._1, x._2 * 10))
+//    val res001: RDD[(String, Int)] = res1.mapValues(x=>x*10)
+    val res002: RDD[(String, Iterable[Int])] = res001.groupByKey()
+    res002.foreach(println)
+
+    while (true) {
+
+    }
   }
 }
